010c18181500f237b61eae717023de32072b2441
Simona Peneva <peneva.simona@gmail.com>
2018-06-19 13:58:36 +0300

    Set up; Remove unnecessary code

---
+++ src/Componentify.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import styled from "styled-components";
+
+class Componentify extends Component {
+  render() {
+    return <div className="App" />;
+  }
+}
+
+export default Componentify;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Componentify from './Componentify';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Componentify />, document.getElementById('root'));
 registerServiceWorker();
