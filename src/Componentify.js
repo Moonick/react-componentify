1acffad0fe135cffe817481ce85efa9c225a99b0
Simona Peneva <peneva.simona@gmail.com>
2018-06-28 17:58:57 +0300

    Change API; Nested components available; WIP

--- src/Componentify.js
+++ src/Componentify.js
@@ -1,9 +1,100 @@
 import React, { Component } from "react";
-import styled from "styled-components";
 
 class Componentify extends Component {
+  getPlainTextComponent(text) {
+    return <span>{text}</span>;
+  }
+
+  getCurrentMatcher(matchersKeys, text) {
+    const { matchers } = this.props;
+
+    return matchersKeys.reduce((acc, key) => {
+      const matcher = Object.assign({}, matchers[key]);
+      const regex = new RegExp(matcher.regex, "g");
+      const match = regex.exec(text);
+
+      if (match !== null) {
+        matcher.match = match;
+        const index = acc && acc.match.index;
+        const currentIndex = matcher.match.index;
+
+        if (!acc || index > currentIndex) {
+          acc = matcher;
+        }
+      }
+
+      return acc;
+    }, null);
+  }
+
+  generateComponent(matcher) {
+    const { component, props, match } = matcher;
+    console.log("matcher", matcher);
+
+    if (typeof props === "function") {
+      return React.createElement(
+        component,
+        props(match),
+        this.generateComponentList(match[1])
+      );
+    }
+
+    return React.createElement(
+      component,
+      props,
+      this.generateComponentList(match[1])
+    );
+  }
+
+  generateComponentList(text) {
+    const { matchers } = this.props;
+    let matchersKeys = Object.keys(matchers);
+    // const index = matchersKeys.indexOf(key);
+
+    // if (matchersKeys.indexOf(key) !== -1) {
+    //   matchersKeys = matchersKeys.slice(index, 1);
+    // }
+
+    // console.log("matchersKeys", matchersKeys);
+    // console.log("key", key);
+    // console.log("index", index);
+
+    let str = text;
+    let components = [];
+    let hasMatcher =
+      matchersKeys.filter(key => matchers[key].regex.exec(str)).length !== 0;
+
+    while (hasMatcher && str !== "") {
+      const currentMatcher = this.getCurrentMatcher(matchersKeys, str);
+
+      if (!currentMatcher) {
+        console.log("bye");
+        break;
+      }
+
+      const matchIndex = currentMatcher.match.index;
+      const textBeforeMatch = str.slice(0, matchIndex);
+      const textAfterMatch = str.slice(
+        matchIndex + currentMatcher.match[0].length
+      );
+      str = textAfterMatch;
+      console.log("hey");
+      components.push(
+        this.getPlainTextComponent(textBeforeMatch),
+        this.generateComponent(currentMatcher)
+      );
+    }
+
+    if (str !== "") {
+      components.push(this.getPlainTextComponent(str));
+    }
+
+    return components;
+  }
+
   render() {
-    return <div className="App" />;
+    const { text } = this.props;
+    return this.generateComponentList(text, null);
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -1,8 +1,42 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import Componentify from './Componentify';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import Componentify from "./Componentify";
+import registerServiceWorker from "./registerServiceWorker";
 
-ReactDOM.render(<Componentify />, document.getElementById('root'));
+const MyComponent = ({ text }) => {
+  return <span style={{ fontStyle: "italic" }}>{text}</span>;
+};
+
+ReactDOM.render(
+  <Componentify
+    text="*haha bold1 _italic_* is *this _love_*"
+    matchers={{
+      bold: {
+        regex: /\*([\w\d\s]+)\*/,
+        component: "span",
+        props: {
+          style: { fontWeight: "900" }
+        }
+      },
+      link: {
+        regex: /(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9]\.[^\s]{2,})/,
+        component: "a",
+        props: ([_, url]) => {
+          return { href: url, targer: "_blank" };
+        }
+      },
+      italic: {
+        regex: /\_([\w\d]+)\_/,
+        component: MyComponent,
+        props: ([_, text]) => {
+          return {
+            text: text
+          };
+        }
+      }
+    }}
+  />,
+  document.getElementById("root")
+);
 registerServiceWorker();
