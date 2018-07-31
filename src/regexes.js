a010bd63a3acff22b34610b41dcd6d4041ca6716
Simona Peneva <peneva.simona@gmail.com>
2018-07-31 11:45:16 +0300

    Prepare before publish

--- src/regexes.js
+++ src/regexes.js
@@ -1,4 +1,6 @@
-export const LINK_REGEX = /(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?(?:\[(.+)\])?/;
-export const BOLD_REGEX = /\*([\w\d\s\:\/\.\[\]]+)\*/;
-export const ITALIC_REGEX = /\_([\w\d\s\:\/\.\[\]]+)\_/;
-export const BR_REGEX = /<br\/>/;
+export const LINK_REGEX = new RegExp(
+  "(https?:\\/\\/)?([\\da-z\\.-]+)\\.([a-z\\.]{2,6})([\\/\\w \\.-]*)*\\/?(?:\\[(.+)\\])?"
+);
+export const BOLD_REGEX = new RegExp("\\*([\\w\\d\\s\\:\\/\\.\\[\\]]+)\\*");
+export const ITALIC_REGEX = new RegExp("\\_([\\w\\d\\s\\:\\/\\.\\[\\]]+)\\_");
+export const BR_REGEX = new RegExp("<br\\/>");
